7c25247377a14042d4be9329c6ea4ded52eec4c9
Thomas Facos <tjfacos@gmail.com>
2023-03-16 14:23:36 +0000

    Changed main.go

--- server/main.go
+++ server/main.go
@@ -1,13 +1,30 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":3000"
+	} else {
+		port = ":" + port
+	}
+
+	return port
+}
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Test")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Hello, Railway!",
+		})
+	})
 
-    app.Listen(":3000")
+	app.Listen(getPort())
 }
