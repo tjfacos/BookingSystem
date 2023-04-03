535d56c076bb95b227cbd284c388581208078b32
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-04-03 20:44:59 +0100

    Its a react app (yay.)

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from "./login/Login"
+
+function App() {
+  return (
+    
+    <Login/>
+
+
+  );
+}
+
+export default App;
---
+++ client/src/api/axios.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: ""  // Time to get a back end mudder-fukker!!!
+})
