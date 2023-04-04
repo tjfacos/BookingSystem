03f45ab9574245c49b3779c9b648bc1616e91503
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-04-04 11:07:09 +0100

    Wayhey, it connects to Planetscale!

--- server/main.py
+++ server/main.py
@@ -1,1 +1,28 @@
-print("hello")
+from dotenv import load_dotenv
+import os
+import mysql.connector
+
+
+load_dotenv()
+
+print(os.getenv("HOST"))
+print(os.getenv("USERNAME"))
+print(os.getenv("PASSWORD"))
+
+
+db = mysql.connector.connect(
+    host= os.getenv("HOST"),
+    user=os.getenv("USERNAME"),
+    passwd= os.getenv("PASSWORD"),
+    ssl_verify_identity=True,
+    ssl_ca="cacert.pem"
+)
+
+cursor = db.cursor()
+
+statement = "INSERT INTO Events (name, host, attendee_no) values (%s, %s, %s)"
+val = ("localEvent", 7, 13)
+
+cursor.execute(statement, val)
+
+db.commit()
