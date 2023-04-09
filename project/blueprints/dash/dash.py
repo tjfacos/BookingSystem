2d361500e7e228fdd0460f4e677f204f89f32b9e
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-04-09 21:36:04 +0100

    Small tweak to change password view

--- project/blueprints/dash/dash.py
+++ project/blueprints/dash/dash.py
@@ -28,9 +28,10 @@ def GuestDashboard():
 
     return render_template("GuestDashboard.html", info=db.getUserInfo(g.user))
 
-@bp.route("/change-password-guest", methods=["POST"])
-def ChangeGuestPassword():
+@bp.route("/change-password", methods=["POST"])
+def ChangePassword():
     new_password = request.form["new-password"]
-    print(new_password)
+    old_password = request.form["old-password"]
+    print(new_password, old_password, g.user["type"])
     flash("Success! Password has been changed!")
     return redirect(url_for("dash.GuestDashboard"))
